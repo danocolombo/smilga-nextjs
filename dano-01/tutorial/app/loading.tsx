bfc5b111ea9092185e0c324c642f7b61184e68db
Dano <danocolombo@gmail.com>
2024-12-06 05:49:10 -0500

    error and loading pages added to tours feature

---
+++ dano-01/tutorial/app/loading.tsx
@@ -0,0 +1,6 @@
+'use client';
+
+function loading() {
+    return <span className='text-xl capitalize'>loading your request...</span>;
+}
+export default loading;
---
+++ dano-01/tutorial/app/tours/loading.tsx
@@ -0,0 +1,6 @@
+'use client';
+
+function loading() {
+    return <span className='text-xl capitalize'>fetching tours data...</span>;
+}
+export default loading;
